f548bcaca50d1cb598a6d0329a943b91769fe6d0
manya706 <sharma.manya721@gmail.com>
2023-09-24 09:04:23 +0530

    added disease outbreak and changed layout

--- Backend/Monitoring-system.py
+++ Backend/Monitoring-system.py
@@ -1,128 +1,43 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 
-# Load the CSV data
 df = pd.read_csv('predictions.csv')
 
-# Define a color palette for diseases
 disease_color_palette = px.colors.qualitative.Plotly
-
-# Map diseases to colors
 disease_to_color = {disease: color for disease, color in zip(df['Prediction'].unique(), disease_color_palette)}
 
-# Sidebar title
 st.sidebar.title("Monitoring System")
 
-# Main content
+selected_page = st.sidebar.selectbox("Select a Page", ["Request Time Graph", "Diseases by Pincode"])
+
 st.title("Monitoring System Visualization")
 
-# Plot request time graph
-st.header("Request Time Graph")
+# Page: Request Time Graph
+if selected_page == "Request Time Graph":
+    st.write('\n\n')
+    st.header("Request Time Graph")
 
-fig_time_line = go.Figure()
-fig_time_line.add_trace(
-    go.Scatter(
-        x=df.index,
-        y=df['Total Time (ms)'],
-        mode='lines',
-        name='Request Time',
-        line=dict(color='blue')
-    )
-)
-fig_time_line.update_layout(
-    title='Request Time Line Plot',
-    xaxis_title='Index',
-    yaxis_title='Total Time (ms)'
-)
-st.plotly_chart(fig_time_line)
+    fig_time_line = px.line(df, x=df.index, y='Total Time (ms)', title='Request Time Line Plot')
+    st.plotly_chart(fig_time_line)
 
+    fig_time = px.histogram(df, x='Total Time (ms)', nbins=20, title='Request Time Histogram')
+    st.plotly_chart(fig_time)
 
-fig_time = px.histogram(df, x='Total Time (ms)', nbins=20, title='Request Time Histogram')
-st.plotly_chart(fig_time)
+# Page: Diseases by Pincode
+elif selected_page == "Diseases by Pincode":
+    st.write('\n\n')
+    st.header("Diseases by Pincode")
 
-# Filter out rows with missing pincode or prediction
-df_filtered = df.dropna(subset=['Pincode', 'Prediction'])
+    pincode = st.text_input("Enter Pincode", "")
+    pincode = int(pincode) if pincode.isdigit() and len(pincode) == 6 else None
 
-# Group by pincode and prediction, count occurrences
-df_grouped = df_filtered.groupby(['Pincode', 'Prediction']).size().reset_index(name='Count')
-
-# Create a density map
-disease_to_color = {
-    'Acne, or Rosacea': 'blue',
-    'Actinic Keratosis, or other Malignant Lesions': 'green',
-    'Alopecia, or other Hair Diseases': 'red',
-    'Atopic Dermatitis': 'purple',
-    'Bacterial Infections': 'orange',
-    'Benign Tumors': 'pink',
-    'Bullous Disease': 'brown',
-    'Connective Tissue Diseases': 'gray',
-    'Eczema': 'cyan',
-    'Exanthems, or Drug Eruptions': 'magenta',
-    'Fungal Infections': 'yellow',
-    'Healthy or Benign growth': 'lime',
-    'Herpes, HPV, other STDs': 'teal',
-    'Lyme Diseasem, Infestations and Bites': 'olive',
-    'Melanoma Skin Cancer Nevi and Moles': 'navy',
-    'Nail Fungus or other Nail Disease': 'maroon',
-    'Poison Ivy or Contact Dermatitis': 'gold',
-    'Psoriasis, Lichen Planus or related diseases': 'purple',
-    'Systemic Disease': 'lime',
-    'Urticaria Hives': 'cyan',
-    'Vascular Tumors': 'fuchsia',
-    'Vasculitis Photos': 'royalblue',
-    'Warts, or other Viral Infections': 'violet'
-}
-df_grouped['Color'] = df_grouped['Prediction'].apply(lambda disease: disease_to_color.get(disease, 'gray'))
-
-india_lat = 20.5937
-india_lon = 78.9629
-
-st.header("Density Map of India using Pincode and Prediction")
-fig_map = go.Figure()
-
-# Loop through each disease and add a trace for it
-for disease, color in disease_to_color.items():
-    filtered_data = df_grouped[df_grouped['Prediction'] == disease]
-    fig_map.add_trace(
-        go.Densitymapbox(
-            lat=,  # Use Pincode for latitude
-            lon=,  # Use Prediction for longitude
-            z=filtered_data['Count'],
-            radius=50,
-            colorscale='Viridis',
-            colorbar=dict(
-                title='Density',
-                thickness=20,
-            ),
-            name=disease,
-            hoverinfo='all',
-            showscale=False,
-            visible=True
-        )
-    )
-
-fig_map.update_layout(
-    mapbox_style="carto-positron",
-    mapbox_center=dict(
-        lat=india_lat,
-        lon=india_lon
-    ),
-    mapbox_zoom=4,
-)
-
-
-# Add a dropdown to select the disease to display
-disease_options = [disease for disease in disease_to_color.keys()]
-disease_dropdown = st.selectbox('Select Disease:', disease_options, index=0)
-for i in range(len(fig_map.data)):
-    fig_map.data[i].visible = False
-fig_map.data[disease_options.index(disease_dropdown)].visible = True
-
-st.plotly_chart(fig_map)
-
-# Display legend for disease colors
-st.subheader("Disease Colors")
-for disease, color in disease_to_color.items():
-    st.markdown(f'<span style="color:{color}">■</span> {disease}', unsafe_allow_html=True)
+    if pincode is not None:
+        filtered_data = df[df['Pincode'] == pincode]
+        if not filtered_data.empty:
+            fig_histogram = px.histogram(filtered_data, x='Prediction', title=f'Diseases for Pincode: {pincode}')
+            st.plotly_chart(fig_histogram)
+        else:
+            st.info("No data available for the entered pincode.")
+    else:
+        st.info("Please enter a valid 6-digit numeric pincode.")
--- Backend/prototype-data.py
+++ Backend/prototype-data.py
@@ -29,20 +29,30 @@ predictions = [
 ]
 
 # List of Indian pin codes
-pin_codes = ['110001', '400001', '560001', '600001', '700001', '800001', '110002', '400002', '560002', '600002']
+pin_codes = ['110001', '400001', '560001', '600001', '700001', '800001', '110002', '400002', '560002', '600002','201010','201013']
 
 # Generate 60 rows of fake data
 rows = []
 for _ in range(60):
     total_time = round(random.uniform(10, 500), 2)  # Random total time in milliseconds
     pincode = random.choice(pin_codes)  # Random Indian pin code
-    prediction = random.choice(predictions)  # Random prediction from the list
+    num_diseases = random.randint(1, 10)  # Random number of diseases (up to 5)
+    diseases = random.sample(predictions, num_diseases)  # Random diseases from the list
 
     # Append to the rows
-    rows.append([total_time, pincode, prediction])
+    for disease in diseases:
+        rows.append([total_time, pincode, disease])
 
 # Write to CSV
 with open('Backend\predictions.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Total Time (ms)', 'Pincode', 'Prediction'])
     writer.writerows(rows)
+
+# This modified version will randomly assign a variable number of diseases to each pincode, up to a maximum of 5 diseases.
+
+
+
+
+
+
