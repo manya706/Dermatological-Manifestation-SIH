4dedb7ea03393fc380731a787084e5373002fd6e
manya706 <sharma.manya721@gmail.com>
2023-09-24 09:20:20 +0530

    Complete monitoring system

--- Backend/Monitoring-system.py
+++ Backend/Monitoring-system.py
@@ -9,22 +9,32 @@ disease_to_color = {disease: color for disease, color in zip(df['Prediction'].un
 
 st.sidebar.title("Monitoring System")
 
-selected_page = st.sidebar.selectbox("Select a Page", ["Request Time Graph", "Diseases by Pincode"])
+selected_page = st.sidebar.selectbox("Select a Page", ["Request Time Graph", "Diseases by Pincode", "Confidence Histogram of Predictions"])
 
 st.title("Monitoring System Visualization")
 
-# Page: Request Time Graph
 if selected_page == "Request Time Graph":
     st.write('\n\n')
     st.header("Request Time Graph")
 
     fig_time_line = px.line(df, x=df.index, y='Total Time (ms)', title='Request Time Line Plot')
+    fig_time_line.update_traces(line=dict(color='blue'))
+    fig_time_line.update_layout(
+        xaxis_title='Index',
+        yaxis_title='Total Time (ms)',
+        template='plotly_dark',
+    )
     st.plotly_chart(fig_time_line)
 
     fig_time = px.histogram(df, x='Total Time (ms)', nbins=20, title='Request Time Histogram')
+    fig_time.update_traces(marker=dict(color='green'))
+    fig_time.update_layout(
+        xaxis_title='Total Time (ms)',
+        yaxis_title='Count',
+        template='plotly_dark',
+    )
     st.plotly_chart(fig_time)
 
-# Page: Diseases by Pincode
 elif selected_page == "Diseases by Pincode":
     st.write('\n\n')
     st.header("Diseases by Pincode")
@@ -36,8 +46,27 @@ elif selected_page == "Diseases by Pincode":
         filtered_data = df[df['Pincode'] == pincode]
         if not filtered_data.empty:
             fig_histogram = px.histogram(filtered_data, x='Prediction', title=f'Diseases for Pincode: {pincode}')
+            fig_histogram.update_traces(marker_color='purple')
+            fig_histogram.update_layout(
+                xaxis_title='Prediction',
+                yaxis_title='Count',
+                template='plotly_dark',
+            )
             st.plotly_chart(fig_histogram)
         else:
             st.info("No data available for the entered pincode.")
     else:
         st.info("Please enter a valid 6-digit numeric pincode.")
+
+elif selected_page == "Confidence Histogram of Predictions":
+    st.write('\n\n')
+    st.header("Confidence Histogram of Predictions")
+
+    confidence_fig = px.histogram(df, x='Confidence', title='Confidence Histogram')
+    confidence_fig.update_traces(marker_color='cyan')
+    confidence_fig.update_layout(
+        xaxis_title='Confidence',
+        yaxis_title='Count',
+        template='plotly_dark',
+    )
+    st.plotly_chart(confidence_fig)
