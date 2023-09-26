0bc9179d79c758da51939b5541e580251a08a7d8
Asus <Asus@10.12.39.146>
2023-09-26 20:30:46 +0530

    css change of detect page

--- assets/js/detect.js
+++ assets/js/detect.js
@@ -7,7 +7,11 @@
  */
 
 // Start upload preview image
-$(".gambar").attr("src", "/assets/img/img-upload.jpg");
+// $(".gambar").attr("src", "/assets/img/img-upload.jpg");
+$(".gambar").attr(
+  "src",
+  "https://github.com/manya706/Dermatological-Manifestation-SIH/blob/manya-changes-cut-branch-anuj/assets/img/img-upload.jpg?raw=true"
+);
 var $uploadCrop, tempFilename, rawImg, imageId;
 function readFile(input) {
   if (input.files && input.files[0]) {
@@ -68,57 +72,87 @@ $("#imageSelectionForm").on("submit", function (e) {
   // alert("2");
   e.preventDefault();
 
-// Create an array to hold image1 and image2
-var imagesArray = [];
+  // Create an array to hold image1 and image2
+  var imagesArray = [];
 
-// Add image1 to the array
-imagesArray.push($(".item-img")[0].files[0]); // Uploaded image
+  // Add image1 to the array
+  imagesArray.push($(".item-img")[0].files[0]); // Uploaded image
 
-// Fetch the selected image file based on its filename
-var selectedImage = $("input[name='selectedImage']:checked").val();
-fetch("assets/img/WEBSITE_SAMPLES/" + selectedImage)
-  .then(response => response.blob())
-  .then(blob => {
-    // Create a new File object from the blob for image2
-    var selectedImageFile = new File([blob], selectedImage);
-    
-    // Add image2 to the array
-    imagesArray.push(selectedImageFile);
-    
-    // Create a FormData object to store the images array and pincode
-    var formData = new FormData();
-    
-    // Append the images array to the FormData
-    formData.append("files", ...imagesArray);
-    
-    // Add the pincode separately to the FormData
-    var pincode = $("#pincode").val();
-    formData.append("pincode", pincode);
-    
-    // Send the FormData object to the server via a POST request
-    $.ajax({
+  // Fetch the selected image file based on its filename
+  var selectedImage = $("input[name='selectedImage']:checked").val();
+  fetch("assets/img/WEBSITE_SAMPLES/" + selectedImage)
+    .then((response) => response.blob())
+    .then((blob) => {
+      // Create a new File object from the blob for image2
+      var selectedImageFile = new File([blob], selectedImage);
+
+      // Add image2 to the array
+      imagesArray.push(selectedImageFile);
+
+      // Create a FormData object to store the images array and pincode
+      var formData = new FormData();
+
+      // Append the images array to the FormData
+      formData.append("files", ...imagesArray);
+
+      // Add the pincode separately to the FormData
+      var pincode = $("#pincode").val();
+      formData.append("pincode", pincode);
+
+      // Send the FormData object to the server via a POST request
+      $.ajax({
         url: "http://localhost:8000/form-predict/", // Replace with your FastAPI endpoint URL
         type: "POST",
         data: formData,
         processData: false,
         contentType: false,
         success: function (response) {
-            // Handle the response from the server
-                    // Display the output in the "output" <p> element
-        $(".output").html(
-          "Label: " + response.prediction.label + "<br>" +
-          "Confidence: " + response.prediction.confidence + "<br>" +
-          "Execution Time: " + response.exectime);
+          // Handle the response from the server
+          // Display the output in the "output" <p> element
+          $(".output").html(
+            "Label: " +
+              response.prediction.label +
+              "<br>" +
+              "Confidence: " +
+              response.prediction.confidence +
+              "<br>" +
+              "Execution Time: " +
+              response.exectime
+          );
         },
         error: function (error) {
-            // Handle any errors
-            console.error(error);
+          // Handle any errors
+          console.error(error);
         },
+      });
     });
-  });
 
-    // },
+  // },
   // });
 });
 
 // ... Remaining JavaScript code ...
+
+const img = document.querySelector(".row");
+const tiltAmount = 10;
+
+img.addEventListener("mousemove", (e) => {
+  const { clientX, clientY } = e;
+  const { left, top, width, height } = img.getBoundingClientRect();
+  
+  const xOffset = (clientX - left - width / 2) / (width / 2);
+  const yOffset = (clientY - top - height / 2) / (height / 2);
+  
+  const xPos = xOffset * tiltAmount;
+  const yPos = -yOffset * tiltAmount;
+  
+  img.style.transition = "transform 0.1s"; // Add a transition
+  
+  img.style.transform = `perspective(800px) rotateX(${yPos}deg) rotateY(${xPos}deg)`;
+});
+
+img.addEventListener("mouseleave", () => {
+  img.style.transition = "transform 0.5s ease"; // Add a transition
+  img.style.transform = "perspective(800px) rotateX(0deg) rotateY(0deg)";
+});
+
